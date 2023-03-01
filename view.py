02d6adea3b72367ef190baf1af14655f12bd0c48
Rodrigo Magalhaes dos Santos <rodrigo@elsendi.com.br>
2023-02-28 23:01:41 -0300

    Reorganize code

--- view.py
+++ view.py
@@ -6,22 +6,31 @@ from painter import CubePainter
 class View():
 
     def __init__(self, cube):
+        # Initialization
         root = Tk()
         frm = ttk.Frame(root, padding=10)
         frm.grid()
-        buttons_frm = ttk.Frame(frm, padding=5)
-        buttons_frm.grid(column=1, row=0)
-        canvas = Canvas(frm)
-        canvas.grid(column=0, row=0, rowspan=2)
-        self.cube = cube
-        self.painter = CubePainter(cube, canvas)
-        self.buttons_frm = buttons_frm
-        ttk.Label(buttons_frm, text="Faces").grid(row=0, column=0, columnspan=2)
-        self.create_face_move_buttons()
-        ttk.Label(buttons_frm, text="Cube").grid(row=4, column=0, columnspan=2)
-        self.create_cube_rotation_buttons()
+        canvas.grid(column=0, row=0, rowspan=2, pady=5)
         self.canvas = canvas
         self.root = root
+        self.cube = cube
+        self.painter = CubePainter(cube, canvas)
+        # Face movement buttons
+        movement_buttons_frm = ttk.Frame(frm, padding=5)
+        movement_buttons_frm.grid(column=1, row=0)
+        self.movement_buttons_frm = movement_buttons_frm
+        ttk.Label(movement_buttons_frm, text="Faces").grid(row=0, column=0, columnspan=2)
+        self.create_face_move_buttons()
+        # Rotations buttons
+        rotations_frm = ttk.Frame(frm, padding=5)
+        rotations_frm.grid(column=2, row=0)
+        self.rotations_frm = rotations_frm
+        ttk.Label(rotations_frm, text="Cube").grid(row=4, column=0, columnspan=2)
+        self.create_cube_rotation_buttons()
+        # Command Buttons
+        commands_frm = ttk.Frame(frm, padding=5)
+        commands_frm.grid(column=3, row=0)
+        ttk.Label(commands_frm, text="Commands").grid(row=0, column=0, columnspan=2)
 
     def get_canvas_reference(self):
         return self.canvas
@@ -36,7 +45,7 @@ class View():
 
     def create_face_move_button(self, text, face, **grid_args):
         ttk.Button(
-            self.buttons_frm,
+            self.movement_buttons_frm,
             text=text,
             command=lambda: self.cube.rotate_face(
                 Movement(face=face, is_prime=False)
@@ -50,7 +59,7 @@ class View():
 
     def create_cube_rotation_button(self, text, axis, **grid_args):
         ttk.Button(
-            self.buttons_frm,
+            self.rotations_frm,
             text=text,
             command=lambda: self.cube.rotate_cube(axis),
         ).grid(**grid_args)
