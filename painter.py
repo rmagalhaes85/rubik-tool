8a7975b24367099065c78dc07382f456b1be9a6a
Rodrigo Magalhaes dos Santos <rmagalhaes85@gmail.com>
2023-03-06 18:21:09 -0300

    Implement TODOs
    - change face naming conventions to match badmephisto's
    - change qcube face display conventions (front on top, down on bottom)
    - display labels identifying the faces in the qcube renderization

--- painter.py
+++ painter.py
@@ -28,6 +28,8 @@ class Recipe(NamedTuple):
     rel_height: float
     # Width of the tile, relative
     rel_width: float
+    # Flag, show the face label or not
+    show_label: bool = False
 
 
 # listens to the Cube and updates its graphical representation in the tkinter canvas
@@ -36,52 +38,56 @@ class CubePainter:
     qcube_renderization_recipes = [
         # -------------------------------------
         # First line
-        Recipe(face='left', pos=7, rel_top=0, rel_left=0, rel_height=1, rel_width=0.5),
-        Recipe(face='top', pos=7, rel_top=0, rel_left=0.5, rel_height=1, rel_width=1),
-        Recipe(face='top', pos=0, rel_top=0, rel_left=1.5, rel_height=1, rel_width=1),
-        Recipe(face='top', pos=1, rel_top=0, rel_left=2.5, rel_height=1, rel_width=1),
-        Recipe(face='right', pos=1, rel_top=0, rel_left=3.5, rel_height=1, rel_width=0.5),
+        Recipe(face='left', pos=1, rel_top=0, rel_left=0, rel_height=1, rel_width=0.5),
+        Recipe(face='front', pos=7, rel_top=0, rel_left=0.5, rel_height=1, rel_width=1),
+        Recipe(face='front', pos=0, rel_top=0, rel_left=1.5, rel_height=1, rel_width=1),
+        Recipe(face='front', pos=1, rel_top=0, rel_left=2.5, rel_height=1, rel_width=1),
+        Recipe(face='right', pos=7, rel_top=0, rel_left=3.5, rel_height=1, rel_width=0.5),
         # -------------------------------------
         # Second line
-        # ------------------------------------- 
-        Recipe(face='left', pos=0, rel_top=1, rel_left=0, rel_height=1, rel_width=0.5),
-        Recipe(face='top', pos=6, rel_top=1, rel_left=0.5, rel_height=1, rel_width=1),
-        Recipe(face='top', pos=8, rel_top=1, rel_left=1.5, rel_height=1, rel_width=1),
-        Recipe(face='top', pos=2, rel_top=1, rel_left=2.5, rel_height=1, rel_width=1),
-        Recipe(face='right', pos=0, rel_top=1, rel_left=3.5, rel_height=1, rel_width=0.5),
         # -------------------------------------
-        # Third line: the first and last tiles 
+        Recipe(face='left', pos=2, rel_top=1, rel_left=0, rel_height=1, rel_width=0.5),
+        Recipe(face='front', pos=6, rel_top=1, rel_left=0.5, rel_height=1, rel_width=1),
+        Recipe(face='front', pos=8, rel_top=1, rel_left=1.5, rel_height=1, rel_width=1,
+               show_label=True),
+        Recipe(face='front', pos=2, rel_top=1, rel_left=2.5, rel_height=1, rel_width=1),
+        Recipe(face='right', pos=6, rel_top=1, rel_left=3.5, rel_height=1, rel_width=0.5),
+        # -------------------------------------
+        # Third line: the first and last tiles
         # are twice higher
-        # ------------------------------------- 
-        Recipe(face='left', pos=1, rel_top=2, rel_left=0, rel_height=2, rel_width=0.5),
-        Recipe(face='top', pos=5, rel_top=2, rel_left=0.5, rel_height=1, rel_width=1),
-        Recipe(face='top', pos=4, rel_top=2, rel_left=1.5, rel_height=1, rel_width=1),
-        Recipe(face='top', pos=3, rel_top=2, rel_left=2.5, rel_height=1, rel_width=1),
-        Recipe(face='right', pos=7, rel_top=2, rel_left=3.5, rel_height=2, rel_width=0.5),
+        # -------------------------------------
+        Recipe(face='left', pos=3, rel_top=2, rel_left=0, rel_height=2, rel_width=0.5,
+               show_label=True),
+        Recipe(face='front', pos=5, rel_top=2, rel_left=0.5, rel_height=1, rel_width=1),
+        Recipe(face='front', pos=4, rel_top=2, rel_left=1.5, rel_height=1, rel_width=1),
+        Recipe(face='front', pos=3, rel_top=2, rel_left=2.5, rel_height=1, rel_width=1),
+        Recipe(face='right', pos=5, rel_top=2, rel_left=3.5, rel_height=2, rel_width=0.5,
+               show_label=True),
         # -------------------------------------
         # Fourth line: has only three tiles,
         # the corners have printed in above
         # iteration
-        # ------------------------------------- 
-        Recipe(face='front', pos=7, rel_top=3, rel_left=0.5, rel_height=1, rel_width=1),
-        Recipe(face='front', pos=0, rel_top=3, rel_left=1.5, rel_height=1, rel_width=1),
-        Recipe(face='front', pos=1, rel_top=3, rel_left=2.5, rel_height=1, rel_width=1),
+        # -------------------------------------
+        Recipe(face='down', pos=7, rel_top=3, rel_left=0.5, rel_height=1, rel_width=1),
+        Recipe(face='down', pos=0, rel_top=3, rel_left=1.5, rel_height=1, rel_width=1),
+        Recipe(face='down', pos=1, rel_top=3, rel_left=2.5, rel_height=1, rel_width=1),
         # -------------------------------------
         # Fifth line
-        # ------------------------------------- 
-        Recipe(face='left', pos=2, rel_top=4, rel_left=0, rel_height=1, rel_width=0.5),
-        Recipe(face='front', pos=6, rel_top=4, rel_left=0.5, rel_height=1, rel_width=1),
-        Recipe(face='front', pos=8, rel_top=4, rel_left=1.5, rel_height=1, rel_width=1),
-        Recipe(face='front', pos=2, rel_top=4, rel_left=2.5, rel_height=1, rel_width=1),
-        Recipe(face='right', pos=6, rel_top=4, rel_left=3.5, rel_height=1, rel_width=0.5),
+        # -------------------------------------
+        Recipe(face='left', pos=4, rel_top=4, rel_left=0, rel_height=1, rel_width=0.5),
+        Recipe(face='down', pos=6, rel_top=4, rel_left=0.5, rel_height=1, rel_width=1),
+        Recipe(face='down', pos=8, rel_top=4, rel_left=1.5, rel_height=1, rel_width=1,
+               show_label=True),
+        Recipe(face='down', pos=2, rel_top=4, rel_left=2.5, rel_height=1, rel_width=1),
+        Recipe(face='right', pos=4, rel_top=4, rel_left=3.5, rel_height=1, rel_width=0.5),
         # -------------------------------------
         # Sixth line
-        # ------------------------------------- 
-        Recipe(face='left', pos=3, rel_top=5, rel_left=0, rel_height=1, rel_width=0.5),
-        Recipe(face='front', pos=5, rel_top=5, rel_left=0.5, rel_height=1, rel_width=1),
-        Recipe(face='front', pos=4, rel_top=5, rel_left=1.5, rel_height=1, rel_width=1),
-        Recipe(face='front', pos=3, rel_top=5, rel_left=2.5, rel_height=1, rel_width=1),
-        Recipe(face='right', pos=5, rel_top=5, rel_left=3.5, rel_height=1, rel_width=0.5),
+        # -------------------------------------
+        Recipe(face='left', pos=5, rel_top=5, rel_left=0, rel_height=1, rel_width=0.5),
+        Recipe(face='down', pos=5, rel_top=5, rel_left=0.5, rel_height=1, rel_width=1),
+        Recipe(face='down', pos=4, rel_top=5, rel_left=1.5, rel_height=1, rel_width=1),
+        Recipe(face='down', pos=3, rel_top=5, rel_left=2.5, rel_height=1, rel_width=1),
+        Recipe(face='right', pos=3, rel_top=5, rel_left=3.5, rel_height=1, rel_width=0.5),
     ]
 
     def __init__(self, cube, canvas):
@@ -95,7 +101,7 @@ class CubePainter:
     def render_qcube(self):
         default_width_px = 40
         default_height_px = 40
-        for r in CubePainter.qcube_renderization_recipes: 
+        for r in CubePainter.qcube_renderization_recipes:
             logging.debug(f'Will render according to recipe {r=}')
             width_px = r.rel_width * default_width_px
             height_px = r.rel_height * default_height_px
@@ -110,7 +116,13 @@ class CubePainter:
                 *tile_position,
                 fill=COLORS[color],
                 outline='black',
-            ) 
+            )
+            if r.show_label:
+                text_color = 'white' if color in ('R', 'G', 'B') else 'black'
+                self.canvas.create_text(left_px + width_px // 2,
+                                        top_px + height_px // 2,
+                                        fill=text_color,
+                                        text=r.face[0].upper())
 
     def render_unfolded(self):
-        raise Exception('Not implemented') 
+        raise Exception('Not implemented')
